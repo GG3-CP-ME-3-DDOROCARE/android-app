094a6cff75a8140a352bb88b1fa6d6f82ab37981
James <jamesfangyauw@gmail.com>
2023-11-05 14:19:05 +0800

    add login feature

--- app/src/main/java/com/ddorocare/brand_audit/LoginActivity.kt
+++ app/src/main/java/com/ddorocare/brand_audit/LoginActivity.kt
@@ -29,51 +29,11 @@ class LoginActivity : AppCompatActivity() {
         email = findViewById(R.id.edt_email)
         password = findViewById(R.id.edt_password)
         loginButton = findViewById(R.id.btn_login)
-
-//        setMyButtonEnable()
-//        validationEditText()
         setupViewModel()
         setupAction()
 
     }
 
-    private fun setMyButtonEnable() {
-        val email = email.text.toString().trim()
-        val password = password.text.toString().trim()
-        loginButton.isEnabled =
-            email.isNotEmpty() && isEmailValid(email) && password.length > 0 && password.isNotEmpty()
-    }
-
-    fun isEmailValid(email: String): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-    }
-
-
-    private fun validationEditText() {
-        email.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                setMyButtonEnable()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-        })
-
-        password.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                setMyButtonEnable()
-            }
-
-            override fun afterTextChanged(s: Editable) {
-            }
-        })
-    }
 
     private fun setupViewModel() {
         loginViewModel = ViewModelProvider(
@@ -96,18 +56,12 @@ class LoginActivity : AppCompatActivity() {
                             findViewById<ProgressBar>(R.id.pb_loading).visibility = View.GONE
 //                            loginViewModel.saveLogin(user.token)
                             AlertDialog.Builder(this).apply {
-                                setTitle("Yeah!")
-                                setMessage("Anda berhasil login")
-                                setPositiveButton("Lanjut") { _, _ ->
                                     val intent = Intent(context, MainActivity::class.java)
 //                                    intent.putExtra(MainActivity.ID_USER, result.data.token)
                                     intent.flags =
                                         Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                                     startActivity(intent)
                                     finish()
-                                }
-                                create()
-                                show()
                             }
                         }
                         is ResultCustom.Error -> {
